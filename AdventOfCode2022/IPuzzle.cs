ddb28d649617fe84d8f25caec36aee2c18761aaa
Ed McAdams <eatdigital@outlook.com>
2022-12-21 20:01:20 -0500

    day 1

---
+++ AdventOfCode2022/IPuzzle.cs
@@ -0,0 +1,8 @@
+﻿namespace AdventOfCode2022
+{
+    public interface IPuzzle
+    {
+        public string PartOneResult();
+        public string PartTwoResult();
+    }
+}
---
+++ AdventOfCode2022/Puzzle.cs
@@ -0,0 +1,24 @@
+﻿namespace AdventOfCode2022
+{
+    public class Puzzle
+    {
+        private protected readonly IList<string> _exampleInput;
+        private protected readonly IList<string> _mainInput;
+
+        public Puzzle(string puzzleDate)
+        {
+            _exampleInput = LoadExampleInput(puzzleDate);
+            _mainInput = LoadMainInput(puzzleDate);
+        }
+
+        private IList<string> LoadExampleInput(string puzzleDate)
+        {
+            return File.ReadLines($"PuzzleInputs/{puzzleDate}ExampleInput.txt").ToList();
+        }
+
+        private IList<string> LoadMainInput(string puzzleDate)
+        {
+            return File.ReadLines($"PuzzleInputs/{puzzleDate}MainInput.txt").ToList();
+        }
+    }
+}
